78e4d512438c8edf29c0ec78dac07886c7bf984c
chenfeng <mengxiang2019>
2020-08-25 10:57:14 +0800

    add

--- src/bin/work/config/db.rs
+++ src/bin/work/config/db.rs
@@ -1,12 +1,4 @@
-use diesel::{pg::PgConnection, r2d2::{self, ConnectionManager}, MysqlConnection};
-
 pub use boss::db::*;
-
-embed_migrations!();
-
-pub type Connection = PgConnection;
-// pub type Pool = r2d2::Pool<ConnectionManager<Connection>>;
-
 
 pub struct BossDB;
 
@@ -18,22 +10,3 @@ impl BossDB{
     }
 }
 
-pub fn migrate_and_config_db(url: &str) -> Pool {
-    info!("Migrating and configurating database...");
-    let manager = ConnectionManager::<Connection>::new(url);
-    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create pool.");
-    embedded_migrations::run(&pool.get().expect("Failed to migrate."));
-
-    pool
-}
-
-// pub type MysqlPool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
-
-pub fn migrate_and_config_msdb(url: &str) -> MysqlPool {
-    info!("Migrating and configurating database...");
-    let manager = ConnectionManager::<MysqlConnection>::new(url);
-    let pool = r2d2::Pool::builder().build(manager).expect("Failed to create pool.");
-    embedded_migrations::run(&pool.get().expect("Failed to migrate."));
-
-    pool
-}
