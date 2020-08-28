94cc00737069669538528be4c448693e88288edd
chenfeng <mengxiang2019>
2020-08-28 18:27:31 +0800

    add

--- src/bin/work/grpc/mod.rs
+++ src/bin/work/grpc/mod.rs
@@ -5,15 +5,14 @@ use greeter::*;
 use tonic::{transport::Server};
 
 
-pub struct Svr{pub x:i32}
+pub struct Svr;
 
 
 impl Svr {
     pub fn new()->Self{
-        Svr{x:10}
+        Svr
     }
     pub async fn register(&self)-> Result<(), Box<dyn std::error::Error>>{
-        println!("register *******");
         let addr = "[::1]:50051".parse()?;
         let greeter = MyGreeter::default();
         Server::builder()
@@ -21,20 +20,6 @@ impl Svr {
             .serve(addr)
             .await?;
 
-        println!("register 1111");
-        Ok(())
-    }
-
-    pub fn test(&self) -> Result<(), Box<dyn std::error::Error>> {
-        println!("****************hello");
-
-        let addr = "[::1]:50051".parse()?;
-        let greeter = MyGreeter::default();
-        Server::builder()
-            .add_service(GreeterServer::new(greeter))
-            .serve(addr);
-
-        println!("test 1111");
         Ok(())
     }
 }
